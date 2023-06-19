Lundalogik/lime-elements

commit c290470676896cc965e2ac3b487fae6783100e6e

feat(form): add support for different types of date-pickers
fix Lundalogik/crm-feature#1584

src/components/form/widgets/date-picker.ts
@@ -1,6 +1,5 @@
 import moment from 'moment/moment';
 import React from 'react';
-import { DateType } from '../../date-picker/date.types';
 import { WidgetProps } from './types';
 import { LimeElementsWidgetAdapter } from '../adapters';
 
@@ -17,7 +16,7 @@ export class DatePicker extends React.Component {
 
     public render() {
         const props: WidgetProps = this.props;
-        const type = getDateType(props.schema);
+        const additionalProps = getAdditionalProps(props.schema);
 
         return React.createElement(LimeElementsWidgetAdapter, {
             name: 'limel-date-picker',
@@ -27,7 +26,7 @@ export class DatePicker extends React.Component {
             },
             widgetProps: props,
             extraProps: {
-                type: type,
+                ...additionalProps,
             },
         });
     }
@@ -48,18 +47,24 @@ export class DatePicker extends React.Component {
             return;
         }
 
-        const dateString = moment(event.detail).format('YYYY-MM-DD');
+        const formatMapping = {
+            'date-time': 'YYYY-MM-DDTHH:mm:ss',
+            date: 'YYYY-MM-DD',
+            time: 'HH:mm:ss',
+        };
+        const dateString = moment(event.detail).format(
+            formatMapping[props.schema.format]
+        );
         props.onChange(dateString);
     }
 }
 
-function getDateType(schema: any): DateType {
-    const format = schema.format;
-    const mapping = {
-        'date-time': 'datetime',
-        date: 'date',
-        time: 'time',
-    };
+function getAdditionalProps(schema: any) {
+    let props: any = {};
 
-    return mapping[format] || 'datetime';
+    if (schema.lime?.component?.props) {
+        props = schema.lime.component.props;
+    }
+
+    return props;
 }

RamaVenkatSai/Working-Application

commit 5a68e11d369443a3052442b72aa8177e96e1ad6c

fix(form): pass date type for format in date widget
The `type` date picker prop is now set depending on the schema format
(`date-time` or `date`) in the date widget. This has been broken since
c290470676896cc965e2ac3b487fae6783100e6e

For the `time` format, the date widget isn't used.

fix: #1489

src/components/form/widgets/date-only-picker.ts
@@ -1,0 +1,20 @@
+import React from 'react';
+import { DatePicker } from './date-picker';
+import { WidgetProps } from './types';
+
+export const DateOnlyPicker: React.FunctionComponent<WidgetProps> = ({
+    value,
+    ...props
+}) => {
+    // A date-only string is treated as UTC by the Date constructor,
+    // but we want to parse it as local date
+    if (typeof value === 'string' && /-\d\d$/.test(value)) {
+        value += 'T00:00';
+    }
+
+    return React.createElement(DatePicker, {
+        type: 'date',
+        value: value,
+        ...props,
+    });
+};

src/components/form/widgets/date-picker.ts
@@ -2,21 +2,25 @@ import moment from 'moment/moment';
 import React from 'react';
 import { WidgetProps } from './types';
 import { LimeElementsWidgetAdapter } from '../adapters';
+import { DateType } from '@limetech/lime-elements';
 
-export class DatePicker extends React.Component {
+type DateWidgetProps = WidgetProps & {
+    type?: DateType;
+};
+
+export class DatePicker extends React.Component<DateWidgetProps> {
     public refs: any;
     public state = {
         modified: false,
     };
 
-    constructor(public props: WidgetProps) {
+    constructor(public props: DateWidgetProps) {
         super(props);
         this.handleChange = this.handleChange.bind(this);
     }
 
     public render() {
-        const props: WidgetProps = this.props;
-        const additionalProps = getAdditionalProps(props.schema);
+        const { type = 'datetime', ...props } = this.props;
 
         return React.createElement(LimeElementsWidgetAdapter, {
             name: 'limel-date-picker',
@@ -26,7 +30,8 @@ export class DatePicker extends React.Component {
             },
             widgetProps: props,
             extraProps: {
-                ...additionalProps,
+                type: type,
+                ...props.schema.lime?.component?.props,
             },
         });
     }
@@ -63,14 +68,4 @@ export class DatePicker extends React.Component {
         );
         props.onChange(dateString);
     }
-}
-
-function getAdditionalProps(schema: any) {
-    let props: any = {};
-
-    if (schema.lime?.component?.props) {
-        props = schema.lime.component.props;
-    }
-
-    return props;
 }

src/components/form/widgets/index.ts
@@ -1,4 +1,5 @@
 import { Checkbox } from './checkbox';
+import { DateOnlyPicker } from './date-only-picker';
 import { DatePicker } from './date-picker';
 import { InputField } from './input-field';
 import { Select } from './select';
@@ -28,7 +29,7 @@ export type WidgetType =
 export const widgets: Partial<Record<WidgetType, any>> = {
     CheckboxWidget: Checkbox,
     DateTimeWidget: DatePicker,
-    DateWidget: DatePicker,
+    DateWidget: DateOnlyPicker,
     EmailWidget: InputField,
     TextWidget: InputField,
     SelectWidget: Select,